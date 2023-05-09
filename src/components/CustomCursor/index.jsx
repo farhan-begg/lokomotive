0960c45c25de86b97876b8b9f819e14b3c3549fb
farhan-begg <farhan.begg@students.makeschool.com>
2023-05-08 21:07:11 -0700

    setup components

---
+++ src/components/CustomCursor/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CustomCursor = () => {
+  return (
+<>
+</>  )
+}
+
+export default CustomCursor
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './styles.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div>menu</div>
+      <div>Flirty Flowers</div>
+      <div>cart</div>
+
+    </div>
+  )
+}
+
+export default Navbar
